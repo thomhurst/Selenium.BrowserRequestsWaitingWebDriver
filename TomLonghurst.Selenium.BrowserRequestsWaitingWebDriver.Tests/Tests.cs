167321a048df40f7dc2b0c497cf4ecf57680c3a3
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-01-16 20:00:46 +0000

    Selenium WebDriver

---
+++ TomLonghurst.Selenium.BrowserRequestsWaitingWebDriver.Tests/Tests.cs
@@ -0,0 +1,49 @@
+using System.Diagnostics;
+using OpenQA.Selenium.Chrome;
+using TomLonghurst.Selenium.BrowserRequestsWaitingWebDriver.Extensions;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace TomLonghurst.Selenium.BrowserRequestsWaitingWebDriver.Tests;
+
+[FixtureLifeCycle(LifeCycle.InstancePerTestCase)]
+[Parallelizable(ParallelScope.All)]
+public class Tests
+{
+    [OneTimeSetUp]
+    public static void Setup()
+    {
+        new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
+    }
+
+    [Test, Repeat(5)]
+    public void Normal_WebDriver_Doesnt_Wait_And_Update_Title()
+    {
+        using var webdriver = new ChromeDriver();
+
+        var stopWatch = Stopwatch.StartNew();
+        
+        webdriver.Navigate().GoToUrl(Path.GetFullPath("Example.html"));
+       
+        Assert.Multiple(() =>
+        {
+            Assert.That(webdriver.Title, Is.EqualTo("Example"));
+            Assert.That(stopWatch.Elapsed.TotalSeconds, Is.LessThan(5));
+        });
+    }
+    
+    [Test, Repeat(5)]
+    public void Wrapped_WebDriver_Does_Wait_And_Update_Title()
+    {
+        using var webdriver = new ChromeDriver().WithWaitingForBrowserRequests();
+
+        var stopWatch = Stopwatch.StartNew();
+        
+        webdriver.Navigate().GoToUrl(Path.GetFullPath("Example.html"));
+       
+        Assert.Multiple(() =>
+        {
+            Assert.That(webdriver.Title, Is.EqualTo("Updated!"));
+            Assert.That(stopWatch.Elapsed.TotalSeconds, Is.GreaterThanOrEqualTo(5));
+        });
+    }
+}
---
+++ TomLonghurst.Selenium.BrowserRequestsWaitingWebDriver/BrowserRequestsWaitingWebDriver.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.Events;
+using OpenQA.Selenium.Support.UI;
+
+namespace TomLonghurst.Selenium.BrowserRequestsWaitingWebDriver
+{
+    public class BrowserRequestsWaitingWebDriver : EventFiringWebDriver
+    {
+        private readonly TimeSpan? _timeout;
+        private int _pendingRequests;
+        
+        public BrowserRequestsWaitingWebDriver(IWebDriver parentDriver, TimeSpan? timeout = null) 
+            : base(parentDriver ?? throw new ArgumentNullException(nameof(parentDriver)))
+        {
+            _timeout = timeout;
+            
+            var network = parentDriver.Manage().Network;
+
+            network.NetworkRequestSent += (sender, args) =>
+            {
+                Interlocked.Increment(ref _pendingRequests);
+            };
+            
+            network.NetworkResponseReceived += (sender, args) =>
+            {
+                Interlocked.Decrement(ref _pendingRequests);
+            };
+
+            Navigated += (sender, args) => WaitForBrowserRequestsToComplete();
+            NavigatedBack += (sender, args) => WaitForBrowserRequestsToComplete();
+            NavigatedForward += (sender, args) => WaitForBrowserRequestsToComplete();
+            
+            FindingElement += (sender, args) => WaitForBrowserRequestsToComplete();
+            ElementClicked += (sender, args) => WaitForBrowserRequestsToComplete();
+            ElementValueChanged += (sender, args) => WaitForBrowserRequestsToComplete();
+            
+            ScriptExecuting += (sender, args) => WaitForBrowserRequestsToComplete();
+            ScriptExecuted += (sender, args) => WaitForBrowserRequestsToComplete();
+            
+            network.StartMonitoring();
+        }
+
+        ~BrowserRequestsWaitingWebDriver()
+        {
+            Dispose();
+            GC.SuppressFinalize(this);
+        }
+
+        private void WaitForBrowserRequestsToComplete()
+        {
+            var timeout = _timeout ?? WrappedDriver.Manage().Timeouts().PageLoad;
+
+            var wait = new WebDriverWait(WrappedDriver, timeout)
+            {
+                Message = $"Browser requests did not finish within {timeout.TotalSeconds} seconds",
+                Timeout = timeout
+            };
+
+            wait.Until(_ => Thread.VolatileRead(ref _pendingRequests) == 0);
+        }
+    }
+}
